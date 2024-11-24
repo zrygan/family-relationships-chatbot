6022224aa5319e25abb3c08e9d8fb9d9c5015465
EMMAN PUNSALAN <eggpunsalan@gmail.com>
2024-11-24 14:17:25 +0800

    First Commit

--- src/main.py
+++ src/main.py
@@ -1,15 +1,6 @@
 from imports import *
 from rules import *
 
-from src.rules import Prompts
-# TODO: implement this method
-# initializes PROLOG
-# def initialize_prolog(): 
-
-# TODO: implement this method
-# initializes rules
-# def initialize_rules():
-
 # TODO: implement this method
 # asks a query
 # def ask_question():
@@ -45,7 +36,6 @@ def main():
 
         # FIXME: (if the input contains a question mark according to the specs)
         # check if input is a question 
-        # check if input is a question FIXME: (if the input contains a question mark according to the specs)
         if prompt.verify(user_input):
             names = prompt.extract_names(user_input)
             # print(names) //for debugging
@@ -63,17 +53,33 @@ def main():
 
 
 
-        # check if input is a statement (most likely ends with a period)
+
+        # FIXME: (most likely ends with a period)
+        # check if input is a statement 
+        if user_input.endswith("."):
+            # handle statement
+            cleaned_input = prompts.remove_vars_and_consts(user_input)
+            extracted_input = prompts.extract_keywords(user_input)
+
+            print(cleaned_input)
+            print(extracted_input)
+            
+            # check with PROLOG if input is feasible 
+
+        # if YES, add to PROLOG knowledge base
+            # check if input is a statement (most likely ends with a period)
         elif "." in user_input:
-
-            # check with PROLOG if input is feasible
+                # check with PROLOG if input is feasible
             pass
-
                 # if YES, add to PROLOG knowledge base
 
-                    # print prompt that chatbot learned a fact
+        # print prompt that chatbot learned a fact
+                # print prompt that chatbot learned a fact
 
+        # if NO, print an error message
                 # if NO, print an error message
+
+        user_input = input("> ").strip()
 
 if __name__ == "__main__":
     main()
